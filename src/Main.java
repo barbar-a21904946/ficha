5a362acc50bd53bd4a24694c94b023f1b56c2870
Bárbara Daniela Malik <61635416+barbar-a21904946@users.noreply.github.com>
2020-10-11 13:38:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Loja{
+
+}
+class Produtos{
+    String titulo;
+    Float peso;
+    Float preco;
+}
+public class Main {
+    static void adicionarProduto(String titulo, int peso, int preco, int stockInicial){
+
+    }
+    static void removerProduto(String titulo){
+
+    }
+    static String nrProdutos(){
+        return "";
+    }
+    static void adicionarStock(String titulo, int quantidadeAAdicionar){
+
+    }
+    static void removerStock(String titulo, int quantidadeARemover){
+
+    }
+    static String obterStock(String titulo){
+        return "";
+    }
+    static ArrayList<String> obterTitulosProdutos(){
+        return null;
+    }
+    static ArrayList<String> obterInformacaoProdutos(){
+        return null;
+    }
+    static String maisBarato(){
+        return "";
+    }
+    static String precoDoMaisBarato(){
+        return "";
+    }
+    static ArrayList<String> osMaisBaratos(){
+        return null;
+    }
+    static String valorTotalEmLoja(){
+        return null;
+    }
+    static HashMap<String, Integer> faltaDeStock(){
+        return null;
+    }
+    static void reset(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
